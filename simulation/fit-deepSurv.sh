8f9e3f10070add8ed2b6f55fbe0365d39a1a30ca
Wenjie Wang <wjwang.stat@gmail.com>
2018-04-02 23:50:41 -0400

    fit DeepSurv model

---
+++ simulation/fit-deepSurv.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+limit=4
+count=0
+
+echo "[$(date)]: simulation started."
+for ((process = 1; process < 1001; process++))
+do
+    python3 fit-deepSurv.py $process &
+    count=$(($count + 1))
+    echo "[$(date)]: process $process started."
+    if [[ $count -ge $limit ]]
+    then
+        wait
+        count=0
+    fi
+done
+
+echo "[$(date)]: all done."
