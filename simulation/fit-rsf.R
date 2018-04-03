80d06cf29fa23fac843a40888189c6ac99b37f1f
Wenjie Wang <wjwang.stat@gmail.com>
2018-04-02 23:51:24 -0400

    fit random survival forest model

---
+++ simulation/fit-rsf.R
@@ -0,0 +1,35 @@
+################################################################################
+### fitting random survival forest for simulated datasets
+### version controlled by git
+################################################################################
+
+
+## read in arguments
+inArgs <- commandArgs(trailingOnly = TRUE)
+trainID <- as.integer(inArgs[1L])
+
+## source functions and packages
+library(methods)
+source("simu-fun.R")
+need.packages("randomForestSRC")
+
+## read in simulated datasets as the train data and the test data
+testID <- ifelse(trainID == 1L, 1000L, trainID - 1L)
+
+inDir <- "simu-data"
+trainDat <- read.csv(file.path(inDir, paste0(trainID, ".csv")))
+testDat <- read.csv(file.path(inDir, paste0(testID, ".csv")))
+oneFit <- rfsrc(Surv(Time, Event) ~ ., data = trainDat, importance = TRUE)
+pred <- predict.rfsrc(oneFit, newdata = testDat, outcome = "test")
+
+## define output names
+outObjName <- paste0("fit", trainID)
+predObjName <- paste0("pred", trainID)
+assign(outObjName, oneFit)
+assign(predObjName, pred)
+
+## save as RData files
+outDir <- "fit-rsf"
+if (! dir.exists(outDir)) dir.create(outDir)
+save(list = c(outObjName, predObjName),
+     file = file.path(outDir, paste0(trainID, ".RData")))
---
+++ simulation/fit-rsf.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+limit=6
+count=0
+
+echo "[$(date)]: simulation started."
+for ((process = 1; process < 1001; process++))
+do
+    Rscript --vanilla fit-rsf.R $process &
+    count=$(($count + 1))
+    echo "[$(date)]: process $process started."
+    if [[ $count -ge $limit ]]
+    then
+        wait
+        count=0
+    fi
+done
+
+echo "[$(date)]: all done."
